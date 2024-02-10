181c3ef4041e50bd052ec5e13daa87402fc095b6
German Diaz <privatelobo@gmail.com>
2024-02-10 14:30:17 -0500

    Revert "update HelloWorld.java"
    This reverts commit f0876f8ab33e7c171ce80d4bd5bd14b366dc7833.

--- Helloworld.java
+++ Helloworld.java
@@ -3,6 +3,5 @@ public class Helloworld
    public static void main (String[] args)
    {
       System.out.println("Hello World");
-      System.out.println("Program modified, bu able to revert to original version using GitHub Dektop");
    }
 }
